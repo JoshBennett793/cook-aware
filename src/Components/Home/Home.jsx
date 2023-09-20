926258d08f02b4f89730eed9c7f8a163a325ce52
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-19 20:59:33 -0500

    Create recipe state and pass setter to Search

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import './Home.scss'
 import Search from '../Search/Search'
+import Results from '../Results/Results'
+import { useState } from 'react'
 
 export default function Home() {
+  const [recipeData, setRecipeData] = useState([])
+
   return (
     <>
-      <Search />
+      <Search setRecipeData={setRecipeData}/>
+      <main>
+        <h2>Recipes</h2>
+        <Results recipeData={recipeData} />
+      </main>
     </>
   )
 }
