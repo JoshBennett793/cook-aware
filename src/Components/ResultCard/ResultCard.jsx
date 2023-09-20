f3a41d5819f4aa34da847df412ee71feb47abd3d
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-19 22:02:05 -0500

    Validate ResultCard component prop types

---
+++ src/Components/ResultCard/ResultCard.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types'
+
+function ResultCard({ recipe }) {
+  const { label, source, calories, yield: servings, images } = recipe
+  const imageUrl = images.SMALL.url
+  const formattedLabel = label.replace(' recipes', '')
+  const formattedCalories = (calories / servings).toFixed(0)
+
+  return (
+    <>
+      <img
+        src={imageUrl}
+        alt='Thumbnail of cooked dish that pertains to recipe'
+      />
+      <p>{formattedLabel}</p>
+      <p>Source: {source}</p>
+      <p>{formattedCalories} Calories</p>
+    </>
+  )
+}
+
+ResultCard.propTypes = {
+  recipe: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    yield: PropTypes.number.isRequired,
+    images: PropTypes.shape({
+      SMALL: PropTypes.shape({
+        url: PropTypes.string.isRequired
+      })
+    }).isRequired
+  }).isRequired
+}
+
+export default ResultCard
