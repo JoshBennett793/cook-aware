29b74462cbcb4434f9e57999996106d97eb25593
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-19 21:54:09 -0500

    Validate Results component prop types

---
+++ src/Components/Results/Results.jsx
@@ -0,0 +1,40 @@
+import ResultCard from '../ResultCard/ResultCard'
+import PropTypes from 'prop-types'
+
+function Results({ recipeData }) {
+  const recipes = recipeData.hits
+  const results = recipes?.map(({ recipe }) => {
+    console.log(recipe)
+    return <ResultCard recipe={recipe} key={recipe.uri} />
+  })
+
+  return <>{results}</>
+}
+
+Results.propTypes = {
+  recipeData: PropTypes.shape({
+    hits: PropTypes.arrayOf(
+      PropTypes.shape({
+        recipe: PropTypes.shape({
+          uri: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+          images: PropTypes.shape({
+            SMALL: PropTypes.shape({
+              url: PropTypes.string
+            })
+          }),
+          source: PropTypes.string,
+          shareAs: PropTypes.string,
+          yield: PropTypes.number.isRequired,
+          dietLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+          healthLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+          ingredientLines: PropTypes.arrayOf(PropTypes.string).isRequired,
+          calories: PropTypes.number.isRequired,
+
+        }).isRequired
+      })
+    ).isRequired
+  }).isRequired
+}
+
+export default Results
