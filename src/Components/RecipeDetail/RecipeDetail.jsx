3420a1d2b4492044a57839c8348c1c35b498efc1
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-22 11:30:42 -0500

    Format and display ingredients and some nutrition data

--- src/Components/RecipeDetail/RecipeDetail.jsx
+++ src/Components/RecipeDetail/RecipeDetail.jsx
@@ -3,38 +3,128 @@ import { useRecipes } from '../../context/ContextProvider'
 import { useEffect } from 'react'
 
 function RecipeDetail() {
-  const { isDataAvailable, fetchRecipes, recipeData } = useRecipes()
+  const { isDataAvailable, fetchRecipes, recipeData } = useRecipes() // type check
   const { uri } = useParams()
-  console.log('uri from RecipeDetail', uri)
+  console.log(uri)
   const encodedUri = encodeURIComponent(uri)
-  console.log('encoded uri from RecipeDetail: ', encodedUri)
 
   useEffect(() => {
-    'calling '
-    fetchRecipes(undefined, encodedUri) // no query passed to fetch call
-  }, [uri])
+    console.log('refetching?')
+    fetchRecipes(undefined, encodedUri)
+  }, [])
 
-  const recipe = recipeData.hits[0].recipe
+  const recipe = recipeData?.hits?.[0]?.recipe
+
+  const labelAndImg = (
+    <>
+      <h2 className='recipe-detail-label'>{recipe.label}</h2>
+      <div>
+        <img
+          className='recipe-detail-img'
+          src={recipe.images.THUMBNAIL.url}
+          alt={recipe.label}
+        />
+        <p className='full-recipe-link'>
+          See full recipe on: <br></br>
+          <a className='recipe-link' href={recipe.url}>
+            {recipe.source}
+          </a>
+        </p>
+      </div>
+    </>
+  )
+
+  const ingredients = (
+    <>
+      <h3>{recipe.ingredientLines.length} Ingredients</h3>
+      <hr></hr>
+      <ol className='ingredients-list'>
+        {recipe.ingredientLines.map(ing => {
+          return (
+            <li key={ing}>
+              <input type='checkbox' name={ing} id={ing} />
+              <label htmlFor={ing}>{ing}</label>
+              <br></br>
+            </li>
+          )
+        })}
+      </ol>
+    </>
+  )
+
+  const nutrition = (
+    <>
+      <h3>Nutrition</h3>
+      <hr></hr>
+
+      <div className='calories-container'>
+        <div className='calories'>
+          {(recipe.calories / recipe.yield).toFixed(0)}
+          <br></br>
+          <span>CAL/SERV</span>
+        </div>
+
+        <div className='calories-daily-value'>
+          {recipe.totalDaily.ENERC_KCAL.quantity.toFixed(0)} %<br></br>
+          <span>DAILY VALUE</span>
+        </div>
+      </div>
+
+      <hr></hr>
+      <br></br>
+      <p className='recipe-detail-health-labels'>
+        {recipe.healthLabels.join(', ')}
+      </p>
+    </>
+  )
+
+  const fatMacros = Object.keys(recipe.totalNutrients).reduce((acc, key) => {
+    if (key.includes('FA')) {
+      const nutrient = recipe.totalNutrients[key]
+      const dailyValue = recipe.totalDaily[key]
+      acc.push({
+        label: nutrient.label,
+        quantity: `${nutrient.quantity.toFixed(0)}${nutrient.unit}`,
+        dv: dailyValue?.quantity.toFixed(0)
+      })
+      return acc
+    }
+    return acc
+  }, [])
+
+  const macros = (
+    <ul className='fat-macros'>
+      {fatMacros.map(macro => {
+        return (
+          <li key={macro.label}>
+            <span>{macro.label}</span>
+            <span>{macro.quantity}</span>
+            <span>{macro.dv ? `${macro.dv}%` : ''}</span>
+            <hr></hr>
+          </li>
+        )
+      })}
+    </ul>
+  )
 
   return (
     <>
       {isDataAvailable ? (
         <>
-          <section className='recipe-detail-label-and-img'>
-            <p className='recipe-detail-label'>{recipe.label}</p>
-            <div>
-              <img
-                className='recipe-detail-img'
-                src={recipe.images.THUMBNAIL.url}
-                alt={recipe.label}
-              />
-              <p className='full-recipe-link'>
-                See full recipe on: <br></br>
-                <a className='recipe-link' href={recipe.url}>
-                  {recipe.source}
-                </a>
-              </p>
-            </div>
+          <section className='recipe-detail-label-and-img recipe-detail-section'>
+            {labelAndImg}
+          </section>
+
+          <section className='recipe-detail-ingredients recipe-detail-section'>
+            {ingredients}
+          </section>
+
+          <section className='recipe-detail-nutrition recipe-detail-section'>
+            {nutrition}
+          </section>
+
+          <section className='recipe-detail-macros recipe-detail-section'>
+            {macros}
           </section>
         </>
       ) : (
