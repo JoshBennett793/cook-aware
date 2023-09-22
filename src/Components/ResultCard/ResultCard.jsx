d23589fdeb68c0c955b09578c8b02bed6016e1b3
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-21 21:38:34 -0500

    Wrap recipe cards in router Link

--- src/Components/ResultCard/ResultCard.jsx
+++ src/Components/ResultCard/ResultCard.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 function ResultCard({ recipe }) {
   const {
+    uri,
     label,
     calories,
     yield: servings,
     images,
     ingredientLines
   } = recipe
+  const encodedUri = encodeURIComponent(uri)
   const imageUrl = images.SMALL.url
   const regex = /recip(?:e|es)/gi
   const formattedLabel = label.replace(regex, '').trim()
@@ -16,21 +19,24 @@ function ResultCard({ recipe }) {
 
   return (
     <article className='recipe-card'>
-      <img
-        src={imageUrl}
-        alt='Thumbnail of cooked dish that pertains to recipe'
-        className='recipe-img'
-      />
-      <p className='recipe-label'>{formattedLabel}</p>
-      <div className='recipe-text-wrapper'>
-        <p className='recipe-calories'>
-          <span className='recipe-card-num'>{formattedCalories}</span> Calories
-        </p>
-        <div className='divider'>|</div>
-        <p className='recipe-ingredients-num'>
-          <span>{numOfIngredients}</span> Ingredients
-        </p>
-      </div>
+      <Link to={`/recipe/${encodedUri}`} className='recipe-link'>
+        <img
+          src={imageUrl}
+          alt={label}
+          className='recipe-img'
+        />
+        <p className='recipe-label'>{formattedLabel}</p>
+        <div className='recipe-text-wrapper'>
+          <p className='recipe-calories'>
+            <span className='recipe-card-num'>{formattedCalories}</span>{' '}
+            Calories
+          </p>
+          <div className='divider'>|</div>
+          <p className='recipe-ingredients-num'>
+            <span>{numOfIngredients}</span> Ingredients
+          </p>
+        </div>
+      </Link>
     </article>
   )
 }
