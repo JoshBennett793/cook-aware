cd89051948d799755ebe7a1e2a8d747d6ebb563a
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-21 21:41:09 -0500

    Create Route for single recipe detail path

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Search from '../Search/Search'
 import Results from '../Results/Results'
+import RecipeDetail from '../RecipeDetail/RecipeDetail'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Results />}></Route>
           <Route path='/recipes/:query' element={<Results />}></Route>
+          <Route path='/recipe/:uri' element={<RecipeDetail />}></Route>
         </Routes>
       </main>
     </>
