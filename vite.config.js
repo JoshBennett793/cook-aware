f017ad62046b4853f8ef81fc527f3379f612d3b6
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-23 21:56:08 -0500

    Config for deployment access to env variables

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
-import { config } from 'dotenv'
 import react from '@vitejs/plugin-react'
-
-const env = config().parsed
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   define: {
-    'process.env.API_ID': JSON.stringify(env.API_ID),
-    'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    'process.env.API_ID': import.meta.env.VITE_API_ID,
+    'process.env.API_KEY': import.meta.env.VITE_API_KEY
   }
 })
